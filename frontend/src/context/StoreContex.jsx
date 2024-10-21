bdaaa5d77bd6d6f2c8ddda73d0fc9681be5376b2
Sri Ramya <skonise1@binghamton.edu>
2024-10-21 13:40:47 -0400

    loginpop up added

--- frontend/src/context/StoreContex.jsx
+++ frontend/src/context/StoreContex.jsx
@@ -18,6 +18,8 @@ const StoreContextProvider =(props) => {
     const removeFromCart = (itemId) => {
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
     }
+
+    
     useEffect(()=>{
         console.log(cartItems);
     },[cartItems])
